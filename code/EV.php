0e5aa6331ad35a0bd2d5bae1aad6e65cdf19697d
Sebastian Pipping <sping@gentoo.org>
2015-01-24 19:17:58 +0100

    Fix code to work with MediaWiki 1.19.19

--- code/EV.php
+++ code/EV.php
@@ -113,7 +113,7 @@ class EV extends QuickTemplate {
        <?php 			foreach($this->data['personal_urls'] as $key => $item) { ?>
        				<li id="<?php echo Sanitizer::escapeId( "pt-$key" ) ?>"<?php
        					if ($item['active']) { ?> class="active"<?php } ?>><a href="<?php
-       				echo htmlspecialchars($item['href']) ?>"<?php echo $skin->tooltipAndAccesskey('pt-'.$key) ?><?php
+       				echo htmlspecialchars($item['href']) ?>"<?php echo $skin->tooltipAndAccesskeyAttribs('pt-'.$key) ?><?php
        				if(!empty($item['class'])) { ?> class="<?php
        				echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
        				echo htmlspecialchars($item['text']) ?></a></li>
@@ -124,7 +124,7 @@ class EV extends QuickTemplate {
        <form method="get" id="searchform" action="<?php $this->text('searchaction') ?>">
           <div>
             <input type="text" 
-             <?php echo $this->skin->tooltipAndAccesskey('search');
+             <?php echo $this->skin->tooltipAndAccesskeyAttribs('search');
     					if( isset( $this->data['search'] ) ) {
     						?> value="<?php $this->text('search') ?>" <?php } ?> 
     				  name="search" id="search" class="inactive" /> <input type="submit" id="searchsubmit" value="Suchen" />
@@ -179,7 +179,7 @@ class EV extends QuickTemplate {
     				 	&& in_array( $key, array( 'edit', 'watch', 'unwatch' ))) {
     				 		echo $skin->tooltip( "ca-$key" );
     				 	} else {
-    				 		echo $skin->tooltipAndAccesskey( "ca-$key" );
+    				 		echo $skin->tooltipAndAccesskeyAttribs( "ca-$key" );
     				 	}
     				 	echo '>'.htmlspecialchars($tab['text']).'</a></li>';
     				} ?>
@@ -190,23 +190,23 @@ class EV extends QuickTemplate {
               		if($this->data['notspecialpage']) { ?>
               				<li id="t-whatlinkshere"><a href="<?php
               				echo htmlspecialchars($this->data['nav_urls']['whatlinkshere']['href'])
-              				?>"<?php echo $this->skin->tooltipAndAccesskey('t-whatlinkshere') ?>><?php $this->msg('whatlinkshere') ?></a></li>
+              				?>"<?php echo $this->skin->tooltipAndAccesskeyAttribs('t-whatlinkshere') ?>><?php $this->msg('whatlinkshere') ?></a></li>
               <?php
               			if( $this->data['nav_urls']['recentchangeslinked'] ) { ?>
               				<li id="t-recentchangeslinked"><a href="<?php
               				echo htmlspecialchars($this->data['nav_urls']['recentchangeslinked']['href'])
-              				?>"<?php echo $this->skin->tooltipAndAccesskey('t-recentchangeslinked') ?>><?php $this->msg('recentchangeslinked') ?></a></li>
+              				?>"<?php echo $this->skin->tooltipAndAccesskeyAttribs('t-recentchangeslinked') ?>><?php $this->msg('recentchangeslinked') ?></a></li>
               <?php 		}
               		}
               		if(isset($this->data['nav_urls']['trackbacklink'])) { ?>
               			<li id="t-trackbacklink"><a href="<?php
               				echo htmlspecialchars($this->data['nav_urls']['trackbacklink']['href'])
-              				?>"<?php echo $this->skin->tooltipAndAccesskey('t-trackbacklink') ?>><?php $this->msg('trackbacklink') ?></a></li>
+              				?>"<?php echo $this->skin->tooltipAndAccesskeyAttribs('t-trackbacklink') ?>><?php $this->msg('trackbacklink') ?></a></li>
               <?php 	}
               		if($this->data['feeds']) { ?>
               			<li id="feedlinks"><?php foreach($this->data['feeds'] as $key => $feed) {
               					?><span id="<?php echo Sanitizer::escapeId( "feed-$key" ) ?>"><a href="<?php
-              					echo htmlspecialchars($feed['href']) ?>"<?php echo $this->skin->tooltipAndAccesskey('feed-'.$key) ?>><?php echo htmlspecialchars($feed['text'])?></a>&nbsp;</span>
+              					echo htmlspecialchars($feed['href']) ?>"<?php echo $this->skin->tooltipAndAccesskeyAttribs('feed-'.$key) ?>><?php echo htmlspecialchars($feed['text'])?></a>&nbsp;</span>
               					<?php } ?></li><?php
               		}
 
@@ -214,18 +214,18 @@ class EV extends QuickTemplate {
 
               			if($this->data['nav_urls'][$special]) {
               				?><li id="t-<?php echo $special ?>"><a href="<?php echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
-              				?>"<?php echo $this->skin->tooltipAndAccesskey('t-'.$special) ?>><?php $this->msg($special) ?></a></li>
+              				?>"<?php echo $this->skin->tooltipAndAccesskeyAttribs('t-'.$special) ?>><?php $this->msg($special) ?></a></li>
               <?php		}
               		}
 
               		if(!empty($this->data['nav_urls']['print']['href'])) { ?>
               				<li id="t-print"><a href="<?php echo htmlspecialchars($this->data['nav_urls']['print']['href'])
-              				?>"<?php echo $this->skin->tooltipAndAccesskey('t-print') ?>><?php $this->msg('printableversion') ?></a></li><?php
+              				?>"<?php echo $this->skin->tooltipAndAccesskeyAttribs('t-print') ?>><?php $this->msg('printableversion') ?></a></li><?php
               		}
 
               		if(!empty($this->data['nav_urls']['permalink']['href'])) { ?>
               				<li id="t-permalink"><a href="<?php echo htmlspecialchars($this->data['nav_urls']['permalink']['href'])
-              				?>"<?php echo $this->skin->tooltipAndAccesskey('t-permalink') ?>><?php $this->msg('permalink') ?></a></li><?php
+              				?>"<?php echo $this->skin->tooltipAndAccesskeyAttribs('t-permalink') ?>><?php $this->msg('permalink') ?></a></li><?php
               		} elseif ($this->data['nav_urls']['permalink']['href'] === '') { ?>
               				<li id="t-ispermalink"<?php echo $this->skin->tooltip('t-ispermalink') ?>><?php $this->msg('permalink') ?></li><?php
               		}
